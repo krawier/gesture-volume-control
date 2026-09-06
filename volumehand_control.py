92c17811e6137d44028b7b4f7e8845124793611c
unknown <o.krawczyk06@gmail.com>
2026-09-06 21:53:58 +0200

    feat: add volBar

--- volumehand_control.py
+++ volumehand_control.py
@@ -12,6 +12,7 @@ cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
+colorVol = (255,255,255)
 
 detector = htm.handDetector(detectionConf=0.8, maxHands=1)
 
@@ -22,6 +23,8 @@ volRange = volume.GetVolumeRange()
 minVol = volRange[0]
 maxVol = volRange[1]
 area = 0
+volPer = 0
+volBar = 400
 
 while True:
     success, img = cap.read()
@@ -46,7 +49,7 @@ while True:
             #convert volume from lenght to actual volume -> reduce resolutin to make it smoother TODO
 
             volPer = np.interp(lenght, [30,250], [0,100])
-
+            volBar = np.interp(lenght, [30,250], [400,150])
             smoothness = 5
             volPer = smoothness * round(volPer/smoothness)
 
@@ -58,6 +61,11 @@ while True:
             if not fingers[4]:
                         volume.SetMasterVolumeLevelScalar(volPer/100, None)
                         cv2.circle(img, (lineInfo[4], lineInfo[5]), 10, (0, 255, 0), cv2.FILLED)
+                        colorVol = (0,0,0)
+            else:
+                 colorVol = (255,255,255)
+
+
 
 
 
@@ -66,6 +74,15 @@ while True:
 
             if lenght < 25:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 10, (0, 0, 255), cv2.FILLED)
+
+
+        cv2.rectangle(img, (50, 150), (85, 400), (255, 255, 255), 3)
+        cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 255, 255), cv2.FILLED)
+        cv2.putText(img, f'Setting: {int(volPer)}%', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 3)
+
+
+    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
+    cv2.putText(img, f'Set: {int(cVol)}%', (40, 80), cv2.FONT_HERSHEY_COMPLEX, 1, colorVol, 3)
 
     cTime = time.time()
     if cTime - pTime > 0:
